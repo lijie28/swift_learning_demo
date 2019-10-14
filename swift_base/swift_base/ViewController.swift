023ae5b70239cabac04324ca6e72f7388800c03b
Jack <lijie9228@hotmail.com>
2019-10-14 22:03:05 +0900

    add some info

--- swift_base/swift_base/ViewController.swift
+++ swift_base/swift_base/ViewController.swift
@@ -9,92 +9,94 @@
 import UIKit
 
 
-enum Sex : Int {
-    case Man
-    case Woman
-    case Unknow
+
+enum Sex: Int{
+    case man
+    case woman
+    
+    case unknow
 }
 
-enum Country : Int{
-    case China
-    case American
-    case Japan
+enum Country: Int{
+    case china
+    case american
+    case japan
 }
 
 struct People {
-    var age : Int?
-    var sex : Sex? {
-        didSet{
-            switch self.sex {
-            case .Man?:
-                self.like = .Woman
-            case .Woman?:
-                self.like = .Man
+    var age: Int?
+    var sex: Sex? {
+        didSet {
+            switch sex {
+            case .man?:
+                like = .woman
+            case .woman?:
+                like = .man
             default:
-                self.like = .Unknow
+                like = .unknow
             }
         }
     }
-    
-    var like : Sex?
-    var country : Country?
-    mutating func setValue(fromData data: Data) ->People? {
-        if data.count < 3{
+
+    var like: Sex?
+    var country: Country?
+    mutating func setValue(fromData data: Data) -> People? {
+        if data.count < 3 {
             return nil
         }
-        self.age = Int(data[0])
-        self.sex = Sex(rawValue: Int(data[1]))
-        self.country = Country(rawValue: Int(data[2]))
+        age = Int(data[0])
+        sex = Sex(rawValue: Int(data[1]))
+        country = Country(rawValue: Int(data[2]))
         return self
     }
 }
 
-
 struct Traffic {
-    //1 byte
-    var type : TrafficType!
-    
-    var speed: Int? = 0//2 byte
-    
-    var speedUnit : SpeedUnit? = nil
-    
-    init(fromBusWithSpeed speed: Int){
-        self.type = .Bus
+    // 1 byte
+    var type: TrafficType!
+
+    var speed: Int? = 0 // 2 byte
+
+    var speedUnit: SpeedUnit?
+
+    init(fromBusWithSpeed speed: Int) {
+        self.type = .bus
         self.speed = speed
-//        self.speedUnit = speedUnit
+//                self.speedUnit = speedUnit
     }
-    
-//    init(fromBus )
-    
-    init(fromData data: Data){
+
+    //    init(fromBus )
+
+    init(fromData data: Data) {
         if data.count > 4 {
             return
         }
-        self.type = TrafficType(rawValue: Int(data[0]))
-        self.speed = Int(data[1]+data[2])
-        self.speedUnit = SpeedUnit(rawValue: Int(data[3]))
+        type = TrafficType(rawValue: Int(data[0]))
+//        var newData = Data(bytes: [UInt8(data[1])], count: 1)
+        print([UInt8](data))
+        speed = Int(data[1] + data[2])
+        speedUnit = SpeedUnit(rawValue: Int(data[3]))
     }
 }
 
 enum SpeedUnit: Int {
-    case KmH
-    case MH
-    case MMins
+    case khm
+    case mh
+    case mMins
 }
 
 enum TrafficType: Int {
-    case Bus
-    case Airplane
-    case Subway
-    case Bike
+    case bus
+    case airplane
+    case subway
+    case bike
 }
 
-
 class EveryDay: NSObject {
-    var findSth : (People?()) = {
-        someone in
-        print(someone)
-    }
+    //    var findSth : (People?()) = {
+    //        someone in
+    //        print(someone)
+    //    }
 }
 
 class ViewController: UIViewController {
@@ -103,50 +105,47 @@ class ViewController: UIViewController {
 
         testTraffic()
     }
+
+    func change<T>(){
+        let a = T()
+        print("\(T)")
+    }
     
     func testTraffic() {
-        
-//        var data = Data()
-//        data.append(Data(bytes: [UInt8(10)], count: 1))
-//        data.append(Data(bytes: [UInt16(300)], count: 2))
-//        data.append(Data(bytes: [UInt8(2)], count: 1))
-        
-        let bytes : [UInt8] = [0x01,0x2c,0x01,0x02]
+        //        var data = Data()
+        //        data.append(Data(bytes: [UInt8(10)], count: 1))
+        //        data.append(Data(bytes: [UInt16(300)], count: 2))
+        //        data.append(Data(bytes: [UInt8(2)], count: 1))
+
+        let bytes: [UInt8] = [0x01, 0x2C, 0x01, 0x02]
         let data = Data(bytes)
-        
-        for i in 0..<bytes.count {
-            print(bytes[i])
+
+        for index in 0 ..< bytes.count {
+            print(bytes[index])
         }
-        
-        
+
         let traffic = Traffic(fromData: data)
         let type = traffic.type
         let speed = traffic.speed
         let speedUnit = traffic.speedUnit
-        
-        
+
         print("traffic:\(String(describing: type)),\(String(describing: speed)),\(String(describing: speedUnit))")
-        
-        
-        
-        
+
         let traffic2 = Traffic(fromBusWithSpeed: 100)
-        
-        print("traffic2:\(String(describing: traffic2.type)),\(String(describing:  traffic2.speed)),\(String(describing:  traffic2.speedUnit))")
+
+        print("traffic2:\(String(describing: traffic2.type)),\(String(describing: traffic2.speed)),\(String(describing: traffic2.speedUnit))")
     }
-    
-    
+
     func testPeople() {
-        
         var data = Data()
         data.append(Data(bytes: [UInt8(10)], count: 1))
         data.append(Data(bytes: [UInt8(1)], count: 1))
         data.append(Data(bytes: [UInt8(3)], count: 1))
-        
+
         var newPeople = People()
-        
+
         let people = newPeople.setValue(fromData: data)
-        
+
         let age = people?.age
         let sex = people?.sex
         let like = people?.like
@@ -154,8 +153,4 @@ class ViewController: UIViewController {
         //        let country = people.setValue(fromData: data)?.country
         print("people:\(String(describing: age)),\(String(describing: sex)),\(String(describing: like))")
     }
-    
-    
-    
 }
-
